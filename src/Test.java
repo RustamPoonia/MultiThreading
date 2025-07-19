739070d3bfca687024a275badbf6bd25b576cb2f
Rustam <rustampoonia@gmail.com>
2025-07-19 19:59:23 +0530

    Creating thread by both Thread class and Runnable Interface

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+
+//By Extending Thread class
+public class Test extends Thread {
+
+    public void run(){
+        System.out.println("Thread Task");
+
+    }
+//    public void run(int i){
+//        System.out.println(i);
+//    }
+
+//    public void start(){
+//        System.out.println("start method");
+//    }
+
+    public static void main(String[] args) {
+        Test t= new Test();
+//        t.run(1);
+//        t.start(1);
+        t.start();
+    }
+}
---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+public class Test1 implements Runnable{
+    public void run(){
+        System.out.println("Thread task");
+    }
+//    public void start(){
+//        System.out.println("Start method");
+//    }
+
+    public static void main(String[] args) {
+        Test1 t= new Test1();
+        Thread th = new Thread(t);
+        th.start();
+    }
+
+}
