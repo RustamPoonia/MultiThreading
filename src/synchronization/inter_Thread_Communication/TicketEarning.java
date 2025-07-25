9904a772f4eeb43c55e9620936249f24c64fa408
Rustam <rustampoonia@gmail.com>
2025-07-25 22:03:16 +0530

    Learned and Practise Synchronization concept

---
+++ src/synchronization/inter_Thread_Communication/TicketEarning.java
@@ -0,0 +1,29 @@
+package synchronization.inter_Thread_Communication;
+
+public class TicketEarning extends Thread{
+    int totel = 0;
+    public void run(){
+        System.out.println("Total earning is :"+totel);
+        synchronized(this) {
+            for (int i = 0; i <= 10; i++) {
+                totel = totel + 100;
+                this.notify();
+            }
+
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) throws InterruptedException {
+
+        TicketEarning obj = new TicketEarning();
+        obj.start();
+//        obj.join(); this will wait for complete execution of the thread
+             synchronized(obj){
+                 obj.wait();
+                 System.out.println("Total earning is :"+obj.totel);
+             }
+    }
+
+}
